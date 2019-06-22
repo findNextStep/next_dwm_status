6ef75a0e2bf141554fe1e831d56e6174291dbc46
findNextStep <findNextStep@gmail.com>
2019-06-23 00:22:50 +0800

    添加声音模块

--- src/exe/status.cpp
+++ src/exe/status.cpp
@@ -4,12 +4,16 @@
 #include "barBase.hpp"
 #include "barManager.hpp"
 #include "barPerSeconds.hpp"
+#include "volumeBar.hpp"
 
 using namespace std;
 
 int main() {
     ::nextDwmStatus::barManager bm;
+    bm.add_bar(std::unique_ptr<::nextDwmStatus::barBase>(new ::nextDwmStatus::staticBar("")));
+    bm.add_bar(std::unique_ptr<::nextDwmStatus::barBase>(new ::nextDwmStatus::volumeBar()));
     bm.add_bar(std::unique_ptr<::nextDwmStatus::barBase>(new ::nextDwmStatus::timecaller()));
+    bm.add_bar(std::unique_ptr<::nextDwmStatus::barBase>(new ::nextDwmStatus::staticBar("")));
     ::nextDwmStatus::barPerSeconds::main_thread();
     return 0;
 }
