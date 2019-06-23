e048b807fc010423a23f1a8554b646f42a739c92
findNextStep <findNextStep@gmail.com>
2019-06-23 23:14:51 +0800

    添加工具函数run_command获取命令行输出

--- include/barBase.hpp
+++ include/barBase.hpp
@@ -7,6 +7,8 @@
 #include <thread>
 #include <iomanip>
 #include <ctime>
+#include <string.h>
+#include <stdio.h>
 
 namespace nextDwmStatus {
 
@@ -26,5 +28,30 @@ public:
         return status;
     }
 };
+
+
+const std::string
+run_command(const std::string command) {
+    char *p;
+    FILE *fp;
+    char buf[1024];
+
+    if(!(fp = popen(command.c_str(), "r"))) {
+        return "";
+    }
+    p = fgets(buf, sizeof(buf) - 1, fp);
+    if(pclose(fp) < 0) {
+        return "";
+    }
+    if(!p) {
+        return "";
+    }
+    if((p = strrchr(buf, '\n'))) {
+        p[0] = '\0';
+    }
+
+    return buf[0] ? buf : NULL;
+}
+
 
 }
