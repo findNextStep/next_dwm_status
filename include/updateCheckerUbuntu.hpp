513cc22f7939b784b7d77a8e04828d9d05763201
findNextStep <findNextStep@gmail.com>
2019-07-04 11:20:34 +0800

    通过天为单位检查，减小检查的次数

--- include/updateCheckerUbuntu.hpp
+++ include/updateCheckerUbuntu.hpp
@@ -3,11 +3,13 @@
 #include <fstream>
 #include <thread>
 
-// #include <fileWatcher/fileWatcher.h>
+#include <fileWatcher.hpp>
+#include <barManager.hpp>
 
 // get file motify time
 #include <sys/stat.h>
 #include <stdio.h>
+#include <iostream>
 
 namespace nextDwmStatus {
 auto get_file_last_change_time(const std::string &file) {
@@ -16,32 +18,38 @@ auto get_file_last_change_time(const std::string &file) {
     return std::make_pair(attrib.st_mtim.tv_sec, attrib.st_mtim.tv_nsec);
 }
 
-class updateCheckerUbuntu : public barPerSeconds {
+class updateCheckerUbuntu : public barBase {
 private:
     const std::string update_file;
     std::string status;
+    wmj::fileWatcher::fileWatcher watcher;
+    std::thread watch_thread;
 protected:
     static auto get_file_last_change_time_diff(const std::string &file) {
         return time(NULL) - get_file_last_change_time(file).first;
     }
     void check() {
         auto time = get_file_last_change_time_diff(update_file);
-        int day = time/24/60/60;
+        int day = time / 24 / 60 / 60;
         status = "";
-        if (day > 0){
-            status = "update in "+std::to_string(time) + "second ago";
+        if(day > 0) {
+            status = "update in " + std::to_string(day) + "day ago";
         }
-    }
-
-    virtual void per_second_task() {
-        check();
     }
 public:
     virtual const std::string getStatus()const {
         return status;
     }
 
-    updateCheckerUbuntu():update_file("/var/lib/apt/periodic/update-success-stamp"){
+    updateCheckerUbuntu():
+        update_file("/var/lib/apt/periodic/update-success-stamp"),
+        watcher(update_file, [this]() {
+        this->check();
+        barManager::requires_launch();
+    }), watch_thread([this]() {
+        this->watcher.run();
+    }) {
+        this->watcher.startWatch();
         check();
     }
 
